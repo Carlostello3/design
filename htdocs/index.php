a6a2db37673fccd117d9cd26dcc2319d580157fb
Carlos Tello <carlostello@carloss-imac.lan>
2020-09-24 20:28:07 -0400

    initial commit

---
+++ htdocs/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+    <title>CT Designs - Your one stop shop for all design desires</title>
+    </head>
+
+    <body>
+        <?php
+            echo "<p>Hello World!</p>"
+        ?>
+        <a href = "login.php"> Click here to login </a> </br>
+        <a href = "register.php"> Click here to register </a>
+    </body>
+</html>
