bb6ba38c98fc8165191a15eb19762ffb035fb78e
Carlos Tello <carlostello@carloss-imac.lan>
2020-09-29 23:23:28 -0400

    added items to generate table grid on home screen

--- htdocs/home.php
+++ htdocs/home.php
@@ -18,5 +18,29 @@
         <h2>Home Page</h2>
         <p>Welcome <?php Print "$username"?>!</p>
         <a href="logout.php">Log Out</a>
+        <div class="main_grid">
+        <?php
+        $servername = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "dbPassword";
+        $dbName = "design_items";
+
+        $conn = new mysqli($servername, $dbUsername, $dbPassword, $dbName);
+
+        if($conn -> connect_error){
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT * FROM products";
+        $result = $conn -> query($sql);
+
+        if($result->num_rows > 0){
+            echo "some results";
+        } else {
+            echo "0 results";
+        }
+
+        ?>
+        </div>
     </body>
 </html>
