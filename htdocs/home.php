1e542efe4c0947f165c44bc05fc53dcb8aee53fc
Carlos Tello <carlostello@carloss-imac.lan>
2020-10-22 17:44:08 -0400

    added tables, updated PDF creation

--- htdocs/home.php
+++ htdocs/home.php
@@ -1,3 +1,4 @@
+
 <html>
     <head>
         <title>CT Designs - Home</title>
@@ -92,85 +93,35 @@
         <br>
         <div id=cart>
 
-            <table >
+            <table>
             
             <tr>
                 <th>Quantity</th>
                 <th>Item</th>
                 <th>Cost</th>
             </tr>
-            <tbody id=tableCart>
-                <script>
-
-                    //ADD ITEMS TO CART
-                    var cart = []
-                    var total = []
-                
-                    function addToCart(element){
-                        var row = element.parentNode.parentNode.firstElementChild;
-                        var name = row.innerHTML;
-                        var price = row.nextElementSibling.innerHTML;
-                        
-                        
-                        cart.push(["1x", name, price])
-                        total.push(parseInt(price.replace('$ ', '')))
-                        
-                        var totalValue = document.getElementById("totalValue").innerHTML
-                        totalValue = "$ " + total.reduce(addTotal) + ".00"
-                        updateCartTable(cart)
-                    }
-
-                    //ADD ITEMS TO TABLE
-                    var tableBody = document.getElementById('tableCart');
-                    
-                    function updateCartTable(cart){
-                        tableBody.innerHTML = '';
-                        cart.forEach(function(rowData){
-                            var row = document.createElement('tr');
-
-                            rowData.forEach(function(cellData){
-                                var cell = document.createElement('td');
-                                cell.appendChild(document.createTextNode(cellData));
-                                row.appendChild(cell);
-                            });
-                        
-                            tableBody.appendChild(row);
-                        });
-                    }
-
-                    //ADD TOTAL VALUE
-                    function addTotal(values, num){
-                        return values + num
-                    }
-
-                    //CLEAR CART
-                    function emptyCart(){
-                        tableBody.innerHTML = '';
-                        totalValue.innerHTML = "$0.00";
-                        cart = [];
-                        total = [];
-
-                    }
-                </script>
-            </tbody>
+            <tbody id=tableCart></tbody>
             <tfooter>
             <tr>
                 <td>TOTAL</td>
                 <td></td>
-                <td id=totalValue></td>
+                <td id=totalValue>$0.00</td>
             </tr>
             
             </tfooter>
             
             </table>
-            <button type="button" onclick="emptyCart()">Clear Cart</button>
+            
         </div>
-
+        <button id="clear" type="button" onclick="emptyCart()">Clear Cart</button>
+        <button id="print" type="button" onclick="printInvoice()">Print Invoice</button>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
         <script>
-
+            
             var cart = []
             var total = []
         
+            //ADD ITEMS TO TABLE
             function addToCart(element){
                 var row = element.parentNode.parentNode.firstElementChild;
                 var name = row.innerHTML;
@@ -184,7 +135,77 @@
                 updateCartTable(cart)
             }
 
+            //CREATE TABLE
+            var tableBody = document.getElementById('tableCart');
+                    
+            function updateCartTable(cart){
+                tableBody.innerHTML = '';
+                cart.forEach(function(rowData){
+                    var row = document.createElement('tr');
+
+                    rowData.forEach(function(cellData){
+                        var cell = document.createElement('td');
+                        cell.appendChild(document.createTextNode(cellData));
+                        row.appendChild(cell);
+                    });
+                
+                    tableBody.appendChild(row);
+                });
+            }
+
+            //ADD TOTAL VALUE
+            function addTotal(values, num){
+                return values + num
+            }
+
+            //CLEAR CART
+            function emptyCart(){
+                tableBody.innerHTML = '';
+                totalValue.innerHTML = "$0.00";
+                cart = [];
+                total = [];
+
+            }
+
+            //PRINT INVOICE PDF
             
+            var pdf = new jsPDF('p', 'pt', 'letter');
+
+            function printInvoice(){
+
+                var logo = new Image();
+                
+                //HEADER
+                logo.onload = function(){ 
+                    alert("Invoice is Ready")
+                }
+
+                logo.onerror = function(){
+                    alert("Erro creating Invoice, Please try again")
+                }
+                logo.src = "images/ctLogo.png"
+                pdf.addImage(logo, "PNG", 15, 15, 178, 109);
+                
+                pdf.setLineWidth(1);
+                pdf.line(25, 120, 590, 120);
+                
+                pdf.text("Customer #: " + totalValue.innerHTML, 282, 50 )
+                
+                
+
+
+                //BODY
+                var cartTable = document.getElementById("cart");
+                pdf.fromHTML(cartTable, 15, 250,{
+                    width: 500
+                });
+
+                //FOOTER
+
+
+
+                pdf.save("Invoice.pdf");
+            }
         </script>
             
         <br>
